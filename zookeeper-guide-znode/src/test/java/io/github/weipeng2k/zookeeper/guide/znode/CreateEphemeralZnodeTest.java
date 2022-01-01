76e19450597f99e11cbc0e63047459e45178d767
weipeng2k <weipeng2k@126.com>
2022-01-01 23:22:52 +0800

    feat(zk-guide): create persistence sequence node.

---
+++ zookeeper-guide-znode/src/test/java/io/github/weipeng2k/zookeeper/guide/znode/CreateEphemeralZnodeTest.java
@@ -0,0 +1,7 @@
+package io.github.weipeng2k.zookeeper.guide.znode;
+
+/**
+ * @author weipeng2k 2022年01月01日 下午23:18:51
+ */
+public class CreateEphemeralZnodeTest {
+}
--- zookeeper-guide-znode/src/test/java/io/github/weipeng2k/zookeeper/guide/znode/CreatePersistenceZnodeTest.java
+++ zookeeper-guide-znode/src/test/java/io/github/weipeng2k/zookeeper/guide/znode/CreatePersistenceZnodeTest.java
@@ -35,4 +35,29 @@ public class CreatePersistenceZnodeTest {
         zooKeeper.delete("/create-persistence-test", 0);
     }
 
+    @Test
+    public void create_persistence_serial_znode() throws InterruptedException, KeeperException {
+        String node1 = zooKeeper.create("/create-persistence-serial-test", "data".getBytes(StandardCharsets.UTF_8),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+
+        String node2 = zooKeeper.create("/create-persistence-serial-test", "data".getBytes(StandardCharsets.UTF_8),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+
+        System.out.println(node1);
+        System.out.println(node2);
+
+        zooKeeper.delete(node1, 0);
+        zooKeeper.delete(node2, 0);
+    }
+
+    @Test(expected = KeeperException.class)
+    public void create_persistence_ttl_znode() throws InterruptedException, KeeperException {
+        String ret = zooKeeper.create("/create-persistence-ttl", "data".getBytes(StandardCharsets.UTF_8),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_WITH_TTL, null, 1000 * 30);
+
+        System.out.println(ret);
+
+        zooKeeper.delete(ret, 0);
+    }
+
 }
